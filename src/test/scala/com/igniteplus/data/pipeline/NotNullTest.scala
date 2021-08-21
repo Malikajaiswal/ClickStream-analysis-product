389741cbd21e785a8d11927fbf1b156bd461de76
Malikajaiswal <68912714+Malikajaiswal@users.noreply.github.com>
2021-08-21 15:06:20 +0530

    updata test case

--- src/test/scala/com/igniteplus/data/pipeline/NotNullTest.scala
+++ src/test/scala/com/igniteplus/data/pipeline/NotNullTest.scala
@@ -1,7 +1,7 @@
 package com.igniteplus.data.pipeline
 
 import com.igniteplus.data.pipeline.Service.FileReaderService.readFile
-import com.igniteplus.data.pipeline.TestConstant.constant.{PRIMARY_KEY, testData}
+import com.igniteplus.data.pipeline.TestConstant.constant.{FORMAT, OUTPUT_PATH, PRIMARY_KEY, TEST_DATA_SAMPLE}
 import com.igniteplus.data.pipeline.cleanser.cleanser.notNullDataframe
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
@@ -22,14 +22,23 @@ class NotNullTest extends  FunSuite with BeforeAndAfterAll {
   }
 
   test("Check Null value in Primary Key Column"){
-    val dftestdata: DataFrame = readFile(testData,"csv","testoutput")(spark)
-//    dftestdata.show()
+
+    // READING DATA FROM SAMPLE INPUT - CONSIST OF 1 + 7 ROWS(2 WITH NULL + 5 WITHOUT NULL)
+    val dftestdata: DataFrame = readFile(TEST_DATA_SAMPLE,FORMAT ,OUTPUT_PATH)(spark)
 
     val notNullDf : DataFrame = dftestdata.na.drop(PRIMARY_KEY)
     val notNullDfCount1 : Long =notNullDf.count()
 
-    val notNullDfTest : DataFrame= notNullDataframe(dftestdata,PRIMARY_KEY,"testoutput","csv")(spark)
+    val notNullDfTest : DataFrame= notNullDataframe(dftestdata,PRIMARY_KEY,OUTPUT_PATH,FORMAT)(spark)
     val notNullDfCount2 : Long =notNullDfTest.count()
+
+  /*
+    comparing the count of not null primary key column
+
+    notNullDfCount1 : "GOT BY DIRECTLY DROPPING NULL VALUES"
+    notNullDfCount2 : "GOT BY USING notNullDataframe METHOD"
+
+   */
     val Result : Boolean = notNullDfCount1 === notNullDfCount2
 
     assertResult(expected = true)(Result)
--- src/test/scala/com/igniteplus/data/pipeline/TestConstant/constant.scala
+++ src/test/scala/com/igniteplus/data/pipeline/TestConstant/constant.scala
@@ -4,8 +4,10 @@ import com.igniteplus.data.pipeline.constant.Constant.{SESSION_ID, VISITOR_ID}
 
 object constant {
 
-  val testData: String ="testInput/demo.csv"
+  val TEST_DATA_SAMPLE: String ="testInput/demo.csv"
+  val OUTPUT_PATH: String ="testoutput"
   val PRIMARY_KEY :Seq[String] = Seq(SESSION_ID, VISITOR_ID )
+  val FORMAT : String = "csv"
 
 
 }
